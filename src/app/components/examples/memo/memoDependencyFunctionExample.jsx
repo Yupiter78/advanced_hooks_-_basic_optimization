9b56b7d3dcb232656b7e52bede4aec13374a666a
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-10-24 23:18:45 +0300

    advanced_hooks react.memo_part_2__6

--- src/app/components/examples/memo/memoDependencyFunctionExample.jsx
+++ src/app/components/examples/memo/memoDependencyFunctionExample.jsx
@@ -1,24 +1,5 @@
-import React, { useEffect } from "react";
-import PropTypes from "prop-types";
-
-const LogOutButton = ({ onLogOut }) => {
-    useEffect(() => {
-        console.log("render button");
-    });
-    return (
-        <button className="btn btn-primary" onClick={onLogOut}>
-            {" "}
-        </button>
-    );
-};
-LogOutButton.propTypes = {
-    onLogOut: PropTypes.func
-};
 const MemoDependencyFunctionExample = (props) => {
-    const handleLogOut = () => {
-        localStorage.removeItem("auth");
-    };
-    return <LogOutButton onLogOut={handleLogOut} />;
+    return null;
 };
 
 export default MemoDependencyFunctionExample;
